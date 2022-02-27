930945d640f60038bdb57edd8877fe4c07f04c3d
dnhorw <dnhorw@gmail.com>
2022-02-26 22:53:20 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { ref } from 'vue'
+import VillagerList from './components/VillagerList.vue'
+
+const newVillagerText = ref('')
+const villagers = ref([
+  {
+    id: 1,
+    title: 'Carl'
+  },
+  {
+    id: 2,
+    title: 'Bungo'
+  },
+  {
+    id: 3,
+    title: 'Gumpy'
+  }
+])
+
+let nextVillagerId = 4
+
+function addNewVillager() {
+  villagers.value.push({
+    id: nextVillagerId++,
+    title: newVillagerText.value
+  })
+  newVillagerText.value = ''
+}
+</script>
+
+<template>
+<form v-on:submit.prevent="addNewVillager">
+<label for="new-villager">Add a villager: </label>
+<input
+v-model="newVillagerText"
+id="new-villager"
+placeholder="E.g. Charles the Horse"
+/>
+<button>Add</button>
+</form>
+<ul>
+  <villager-item
+    v-for="(villager, index) in villagers"
+    :key="villager.id"
+    :title="villager.title"
+    @remove="villagers.splice(index, 1)"
+></villager-item>
+</ul>
+  </template>
+
+
+
